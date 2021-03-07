4b38514e7955ccdc4d35e5e7356bd6f3e07002e0
Alan Rabelo Martins <alanrabelo@icloud.com>
2021-03-07 17:29:54 -0300

    Adjustments in StatementViewModel

--- Phi/Phi/BalanceStatement/ViewModel/StatementViewModel.swift
+++ Phi/Phi/BalanceStatement/ViewModel/StatementViewModel.swift
@@ -6,31 +6,65 @@
 //
 
 import Foundation
+import PhiNetwork
+
+protocol StatementDetailsViewModelDelegate: class {
+    func errorLoadingDetails()
+    func didFinishLoadingDetails()
+}
 
 class StatementViewModel {
     private let statement: StatementItem
+    var statementDetails: StatementDetailsViewModel?
     
+    weak var delegate: StatementDetailsViewModelDelegate?
+
     var value: String {
         return (Double(statement.amount ?? 0) / 100.0).monetary
     }
     
     var details: String {
-        return statement.description ?? "Sem descrição"
+        return statement.details ?? "Sem descrição"
     }
     
     var destination: String {
-        statement.to ?? "Sem destinatário"
+        statement.destination ?? "Sem destinatário"
     }
     
     var date: String {
-        statement.createdAt?.short ?? "Sem data informada"
+        statement.date?.short ?? "Sem data informada"
     }
     
     var isPix: Bool {
-        return statement.tType?.lowercased().contains("pix") ?? false
+        return statement.type?.rawValue.lowercased().contains("pix") ?? false
     }
     
     init(statement: StatementItem) {
         self.statement = statement
+    }
+    
+    func loadDetails() {
+        
+        guard let id = self.statement.id else {
+            delegate?.errorLoadingDetails()
+            return
+        }
+        
+        NetworkLayer.shared.perform(route: .details(id)) { [weak self] (result: Result<StatementDetails, Error>) in
+            switch result {
+            case .success(let statementDetails):
+                
+                self?.statementDetails = StatementDetailsViewModel(statementDetails: statementDetails)
+                self?.delegate?.didFinishLoadingDetails()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension StatementViewModel: StatementDetailsViewDelegate {
+    func value(key: StatementDetailsFields) -> String? {
+        statementDetails?.value(field: key)
     }
 }
